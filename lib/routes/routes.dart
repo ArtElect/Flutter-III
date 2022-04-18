0f5e9ca00262f800236e29caedec30b9d01001a2
zhiwen.wang <zhiwen.wang@epitech.eu>
2022-04-18 15:15:14 +0200

    Delete old admin project

--- lib/routes/routes.dart
+++
@@ -1,6 +0,0 @@
-class Routes {
-  static const String signin = "/signin";
-  static const String signup = "/signup";
-  static const String groups = "/groups";
-  static const String users = "/users";
-}
