25d865aba3083efef3326cde4930573b760b03ac
JinyangZ <jinyang.zou@epitech.eu>
2022-04-18 15:42:48 +0200

    [ADD/MOD]: changed route to onGenerateRoute, add home screen file and route

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:client/routes/router.dart';
 import 'package:flutter/material.dart';
 import 'package:client/routes/routes.dart';
-import 'package:client/screens/signin.dart';
-import 'package:client/screens/dashboard.dart';
 import 'package:client/constant/my_colors.dart';
 
 void main() {
@@ -11,32 +10,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Epitech Dashboard',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
           color: MyColors.purple,
         ),
       ),
       initialRoute: Routes.signin,
-      routes: <String, Widget Function(BuildContext)>{
-        Routes.signin: (BuildContext context) => const SignIn(),
-        Routes.dashboard: (BuildContext context) => const Dashboard(),
-      },
+      onGenerateRoute: (setting) => GenerateRoutes().generateRoute(setting),
     );
   }
 }
--- client/lib/routes/routes.dart
+++ client/lib/routes/routes.dart
@@ -1,4 +1,5 @@
 class Routes {
+  static const String home = "/home";
   static const String signin = "/signin";
   static const String dashboard = "/dashboard";
   static const String projects = "/projects";
