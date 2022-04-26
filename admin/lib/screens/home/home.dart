34c26d06649cd45243db0d8b1a65a6a38a4ad45e
hadrienm <adrien.heros@epitech.eu>
2022-04-26 16:51:42 +0200

    Update home.dart

--- admin/lib/screens/home/home.dart
+++ admin/lib/screens/home/home.dart
@@ -57,7 +57,7 @@ class _HomePageState extends State<HomePage> {
                                                     )
                                                 )
                                             ),
-                                            Text(snapshot.data![index].id.toString()),
+                                            Text(snapshot.data![index].id!),
                                           ],
                                         )
                                     );
