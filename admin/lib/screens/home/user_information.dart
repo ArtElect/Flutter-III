1b8f80eb0d4bb71aad567ea8d43d4b0243c87a71
hadrienm <adrien.heros@epitech.eu>
2022-04-27 18:13:59 +0200

    Update user_information.dart

--- admin/lib/screens/home/user_information.dart
+++ admin/lib/screens/home/user_information.dart
@@ -21,56 +21,58 @@ class _UserInformationPageState extends State<UserInformationPage> {
       body: Row(
         children: [
           const Sidebar(selectedIndex: 0),
-          Center(
-            child: Card(
-              child: SizedBox(
-                height: 400,
-                width: 400,
-                child: Column(
-                  children: [
-                    const Padding(padding: EdgeInsets.only(top: 10)),
-                    Container(
-                        width: 100.0,
-                        height: 100.0,
+          Expanded(
+              child: Center(
+                child: Card(
+                child: SizedBox(
+                  height: 400,
+                  width: 400,
+                  child: Column(
+                    children: [
+                      const Padding(padding: EdgeInsets.only(top: 10)),
+                      Container(
+                          width: 100.0,
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage(widget.dbUserModel.image!),
+                              )
+                          )
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        padding: const EdgeInsets.all(20.0),
                         decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: NetworkImage(widget.dbUserModel.image!),
-                            )
-                        )
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.0),
-                        border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(color: Colors.grey),
+                        ),
+                        child: Text("Id: " + widget.dbUserModel.id!),
                       ),
-                      child: Text("Id: " + widget.dbUserModel.id!),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.0),
-                        border: Border.all(color: Colors.grey),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        padding: const EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(color: Colors.grey),
+                        ),
+                        child: Text("User id: " + widget.dbUserModel.userId!),
                       ),
-                      child: Text("User id: " + widget.dbUserModel.userId!),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.0),
-                        border: Border.all(color: Colors.grey),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        padding: const EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(color: Colors.grey),
+                        ),
+                        child: Text("Role: " + widget.dbUserModel.role!),
                       ),
-                      child: Text("Role: " + widget.dbUserModel.role!),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
+            )
           )
         ],
       ),
