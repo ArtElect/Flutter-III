d4859ac8dc44eda83d80f9827ed8010918efb770
Vincent Lemesle <vincent.lemesle@epitech.eu>
2022-05-02 12:48:31 +0200

    Revert "edit: README"
    This reverts commit 0488d92109ffac1078b7643225c5cc1d82aa90e5.

--- functions/seed.ts
+++ functions/seed.ts
@@ -9,42 +9,34 @@ const rights = ['CREATE', 'READ', 'UPDATE', 'DELETE'];
 
 const groups = [{
     name: 'GAME DEV',
-    description: 'GAME DEV group description',
+    description: 'Description du groupe GAME DEV',
     image: 'https://i0.wp.com/hipther.com/wp-content/uploads/2021/11/five-ai-tools-every-game-developer-should-have-in-their-toolbelt.jpg?resize=696%2C463&ssl=1',
   }, {
     name: 'APP DEV',
-    description: 'APP DEV group description',
-    image: 'https://news.gandi.net/wp-content/uploads/2018/11/domain_dev_23222b-1024x512.jpg'
+    description: 'Description du groupe APP DEV',
+    image: 'imageUrl'
   }, {
     name: 'WEB DEV',
-    description: 'WEB DEV group description',
-    image: 'https://news.gandi.net/wp-content/uploads/2018/11/domain_dev_23222b-1024x512.jpg'
+    description: 'Description du groupe WEB DEV',
+    image: 'imageUrl'
   }, {
     name: 'IOT ARCHITECTURE',
-    description: 'IOT ARCHITECTURE group description',
-    image: 'https://www.portices.fr/wp-content/uploads/2021/08/Internet-of-Things-IoT.jpg'
+    description: 'Description du groupe IOT ARCHITECTURE',
+    image: 'imageUrl'
   }, {
     name: 'BLUETOOTH LE',
-    description: 'BLUETOOTH LE group description',
-    image: 'https://www.mokoblue.com/wp-content/uploads/2021/03/%E4%B8%BB%E5%9B%BE-1.webp'
+    description: 'Description du groupe BLUETOOTH LE',
+    image: 'imageUrl'
   }, {
     name: 'MACHINE LEARNING',
-    description: 'MACHINE LEARNING group description',
-    image: 'https://www.jeveuxetredatascientist.fr/wp-content/uploads/2015/01/shutterstock_1092234560-1080x652.jpg'
+    description: 'Description du groupe MACHINE LEARNING',
+    image: 'imageUrl'
   }
 ];
 
 const seedDb = async () => {
   await Promise.all(rights.map(async (r) => admin.firestore().collection('right').add({ action: r })));
   await Promise.all(groups.map(async (g) => admin.firestore().collection('group').add(g)));
-  await admin.firestore().collection('account').add({
-    firstname: 'admin',
-    lastname: 'admin',
-    pseudo: 'admin',
-    image: 'admin',
-    role: 'ADMIN',
-    userId: 'to be set',
-  })
 }
 
 seedDb();
